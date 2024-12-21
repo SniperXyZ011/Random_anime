f60cad8e0adc05666726a1c65d99ca56478e060e
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2024-12-22 00:08:32 +0530

    commited till work

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.use(express.static("public"));
 
 const API_url = "https://api.mangadex.org";
 
+function getChapters(mangaId){
+
+}
+
+
 app.get("/", async (req, res) => {
     const result = await axios(API_url + "/manga/random");
     const val = result.data;
+    console.log(val.data.id);
     const coverRelationship = val.data.relationships.find((obj) => obj.type === "cover_art");
     if (!coverRelationship) {
         throw new Error("Cover art not found for this manga.");
@@ -28,8 +34,20 @@ app.get("/", async (req, res) => {
     });
 });
 
+//implementing logic 01
+
+app.get("/chapters", async (req, res) =>{
+    const mangaId = req.query.mangaId;
+    console.log(mangaId);
+    // const result = await axios(`https://api.magadex.org/manga/${mangaId}/feed`);
+    // const val = result.data;
+    
+})
+
 
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`)
 });
+
+// module.exports = getChapters;
